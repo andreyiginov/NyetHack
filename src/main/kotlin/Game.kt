07f9979370c27e51531f5b7f04eb7e99b997f258
andreyiginov <andreyiginov@gmail.com>
2022-10-30 03:09:53 +0300

    Health has been worked out and aura has been added
     With the help of if/else operators, health levels have been worked out and aura has been added.
    
     Added immortality
    
    The code was refactored

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,9 +1,23 @@
 fun main(args: Array<String>) {
     val name = "Madrigal"
-    var healthPoints = 100
-    if (healthPoints == 100) {
-        println(name + "is in excellent condition!")
-} else {
-    println(name + "is in awful condition!")
-}
+    var healthPoints = 89
+    val isBlessed = true
+    val isImmortal = false
+
+    // Aura
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+
+    val auraColor = if (auraVisible) "GREEN" else "NONE"
+    println(auraColor)
+
+    val healthStatus = if (healthPoints == 100) "is in excellent condition!"
+    else if (healthPoints >= 90) "has a few scratches."
+    else if (healthPoints >= 75)
+        if (isBlessed) "has some minor wounds but is healing quite quickly!"
+        else "has some minor wounds."
+    else if (healthPoints >= 15) "looks pretty hurt."
+    else "is in awful condition!"
+
+    // Player Status
+    println(name + " " + healthStatus)
 }
